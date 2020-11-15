50ae22107a1c8edc1de9ac18d211c5c4d58f84ae
João Vítor <joao.sferreira@souunit.com.br>
2020-11-15 00:08:50 -0300

    fix.

--- exercicio1-web/src/server.js
+++ exercicio1-web/src/server.js
@@ -2,15 +2,7 @@ const express = require("express");
 const app = express();
 app
   .use(express.json())
-  .use(express.static("public"))
-  .use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+  .use(express.static("../public"));
 
 // porta do servidor
 const PORT = 8005;
