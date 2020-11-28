eea95fc0429d6bba8635fe7fe6f7eadc6466bdb2
João Vítor <joao.sferreira@souunit.com.br>
2020-11-28 13:17:40 -0300

    refatoração usando OO, Cliente.js

---
+++ exercicio4/src/Cliente.js
@@ -0,0 +1,10 @@
+class Cliente {
+  constructor(agencia, contaCorrente, nomeCompleto, tel) {
+      this.agencia = agencia;
+      this.contaCorrente = contaCorrente;
+      this.nomeCompleto = nomeCompleto;
+      this.tel = tel;
+  }
+}
+
+module.exports = Cliente;
--- exercicio4/src/banco.js
+++ exercicio4/src/banco.js
@@ -1,15 +1,18 @@
 let express = require("express");
 let router = express.Router();
 
+const Cliente = require("./Cliente.js");
 const db = require("quick.db");
 
 router.post("/cadastrarcliente", function (req, res) {
-  let agencia = req.body.agencia;
-  let contaCorrente = req.body.conta_corrente;
-  let nomeCompleto = req.body.nome_completo;
-  let tel = req.body.tel;
+  let cliente = new Cliente(
+    req.body.agencia,
+    req.body.conta_corrente,
+    req.body.nome_completo,
+    req.body.tel
+  );
 
-  db.set(`conta_${agencia + contaCorrente + nomeCompleto + tel}`, 0);
+  db.set(`conta_${cliente.agencia + cliente.contaCorrente + cliente.nomeCompleto + client.tel}`, 0);
 
   return res.status(200).json({
     Agência: req.body.agencia,
@@ -20,43 +23,49 @@ router.post("/cadastrarcliente", function (req, res) {
 });
 
 router.get("/visualizarconta", function (req, res) {
-  let agencia = req.body.agencia;
-  let contaCorrente = req.body.conta_corrente;
-  let nomeCompleto = req.body.nome_completo;
-  let tel = req.body.tel;
+  let cliente = new Cliente(
+    req.body.agencia,
+    req.body.conta_corrente,
+    req.body.nome_completo,
+    req.body.tel
+  );
 
   return res.status(200).json({
-    Saldo: db.fetch(`conta_${agencia + contaCorrente + nomeCompleto + tel}`),
+    Saldo: db.fetch(`conta_${cliente.agencia + cliente.contaCorrente + cliente.nomeCompleto + cliente.tel}`),
   });
 });
 
 router.get("/realizardeposito", function (req, res) {
-  let agencia = req.body.agencia;
-  let contaCorrente = req.body.conta_corrente;
-  let nomeCompleto = req.body.nome_completo;
-  let tel = req.body.tel;
+  let cliente = new Cliente(
+    req.body.agencia,
+    req.body.conta_corrente,
+    req.body.nome_completo,
+    req.body.tel
+  );
   let deposit = req.body.deposit;
 
-  db.add(`conta_${agencia + contaCorrente + nomeCompleto + tel}`, deposit);
+  db.add(`conta_${cliente.agencia + cliente.contaCorrente + cliente.nomeCompleto + cliente.tel}`, deposit);
 
   return res.status(200).json({
     "Novo Saldo": db.fetch(
-      `conta_${agencia + contaCorrente + nomeCompleto + tel}`
+      `conta_${cliente.agencia + cliente.contaCorrente + cliente.nomeCompleto + cliente.tel}`
     ),
   });
 });
 
-router.put("/realizarsaque", function(req, res) {
-  let agencia = req.body.agencia;
-  let contaCorrente = req.body.conta_corrente;
-  let nomeCompleto = req.body.nome_completo;
-  let tel = req.body.tel;
+router.put("/realizarsaque", function (req, res) {
+  let cliente = new Cliente(
+    req.body.agencia,
+    req.body.conta_corrente,
+    req.body.nome_completo,
+    req.body.tel
+  );
   let saque = req.body.saque;
 
-  db.subtract(`conta_${agencia + contaCorrente + nomeCompleto + tel}`, saque);
+  db.subtract(`conta_${cliente.agencia + cliente.contaCorrente + cliente.nomeCompleto + cliente.tel}`, saque);
   return res.status(200).json({
     "Novo Saldo": db.fetch(
-      `conta_${agencia + contaCorrente + nomeCompleto + tel}`
+      `conta_${cliente.agencia + cliente.contaCorrente + cliente.nomeCompleto + cliente.tel}`
     ),
   });
 });
